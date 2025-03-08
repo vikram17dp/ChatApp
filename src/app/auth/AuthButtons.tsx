8340b86771fe58e685f014858882326321d2fe0c
vikram17dp <vikramdp505@gmail.com>
2025-03-09 00:11:12 +0530

    adding kinde authentication to app

--- src/app/auth/AuthButtons.tsx
+++ src/app/auth/AuthButtons.tsx
@@ -1,13 +1,21 @@
+"use client"
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import React, { useState } from 'react'
 
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
 const AuthButtons = () => {
+  const [isloading,setisLoading] = useState(false)
   return (
     <div className='flex gap-3 w-full md:w-auto md:flex-row flex-col relative z-50'>
-        <Button className='w-full md:w-40 cursor-pointer' variant={"outline"}>
+        <RegisterLink className='flex-1' onClick={()=>setisLoading(true)}>
+        <Button className='w-full md:w-40 cursor-pointer' variant={"outline"} disabled={isloading}>
             Sign Up
         </Button>
-        <Button className='w-full md:w-40 cursor-pointer'>Log In</Button>
+        </RegisterLink>
+        <LoginLink className='flex-1' onClick={()=>setisLoading(true)}>
+
+        <Button className='w-full md:w-40 cursor-pointer' disabled={isloading}>Log In</Button>
+        </LoginLink>
     </div>
   )
 }
---
+++ src/app/auth/callback/page.tsx
@@ -0,0 +1,10 @@
+
+const page = () => {
+  return (
+    <div>
+      authpage
+    </div>
+  )
+}
+
+export default page
