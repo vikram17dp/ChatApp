c6d65ef4f4d1cb0d065a9ab3e7eecd3ff4c6cfb1
vikram17dp <vikramdp505@gmail.com>
2025-03-09 00:49:42 +0530

    useage of tanstackreactquery into project

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,8 +1,14 @@
+import { Loader } from "lucide-react"
 
 const page = () => {
   return (
-    <div>
-      authpage
+    <div className="mt-20 w-full flex justify-center">
+        <div className="flex flex-col items-center gap-2">
+            <Loader className="w-10 h-10 animate-spin text-muted-foreground"/>
+            <h3 className="text-xl font-bold">Redirecting...</h3>
+            <p className="font-semibold text-sm">Please Wait</p>
+        </div>
+      
     </div>
   )
 }
